6b6cd4f2f24f8e9e26e95be6c78f8334f72cbeec
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2022-10-04 12:00:30 +0200

    Update Time with Luxon CDN

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import Library from './modules/functions.js';
 import navigation from './modules/navigation.js';
-import { DateTime } from './modules/luxon.js';
+import * as dateTime from './modules/date.js';
 
-const dt = DateTime.fromObject({ day: 22, hour: 12 }, { zone: 'America/Los_Angeles', numberingSystem: 'beng' });
-const datestring = dt.toLocaleString(DateTime.DATETIME_MED);
 const date = document.querySelector('.our-date');
-date.innerHTML = `${datestring}`;
+date.innerHTML = `${dateTime.date} ${dateTime.hour}:${dateTime.min}:${dateTime.sec}`;
+// import { DateTime } from './modules/luxon.js';
+// const dt = DateTime.fromObject({ day: 22, hour: 12 },
+ { zone: 'America/Los_Angeles', numberingSystem: 'beng' });
+// const datestring = dt.toLocaleString(DateTime.DATETIME_MED);
+// const date = document.querySelector('.our-date');
+// date.innerHTML = `${datestring}`;
 const awesomeBooks = new Library();
 awesomeBooks.addBook();
 awesomeBooks.renderBooks();
